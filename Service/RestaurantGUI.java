92c9142124dbffbfb926aa22b7e3f3dd7c022e4d
jordan007 <tremblay_jordan@yahoo.ca>
2012-12-09 12:46:54 -0500

    Update Service/RestaurantGUI.java

--- Service/RestaurantGUI.java
+++ Service/RestaurantGUI.java
@@ -9,7 +9,7 @@ public class RestaurantGUI extends javax.swing.JFrame {
     ServiceGUI table2;
     ServiceGUI table3;
     ServiceGUI table4;
-
+ 
     public RestaurantGUI() {
         initComponents();
         table1 = new ServiceGUI();
@@ -17,22 +17,24 @@ public class RestaurantGUI extends javax.swing.JFrame {
         table3 = new ServiceGUI();
         table4 = new ServiceGUI();
     }
-
+    
     private void initComponents() {
-    	setUndecorated(true);
+        
+        setUndecorated(true);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int screenWidth = screenSize.width;
         int screenHeight = screenSize.height;
         setBounds(0,0,screenSize.width, screenSize.height);
-        
-        cmdTable2 = new javax.swing.JButton();
-        cmdTable4 = new javax.swing.JButton();
+
+        cmdLogout = new javax.swing.JButton();
         cmdTable1 = new javax.swing.JButton();
+        cmdTable2 = new javax.swing.JButton();
         cmdTable3 = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        cmdTable4 = new javax.swing.JButton();
+        lblLegende = new javax.swing.JLabel();
+        lblOccupe = new javax.swing.JLabel();
+        lblLibre = new javax.swing.JLabel();
+        lblImage = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(800, 830));
@@ -44,87 +46,95 @@ public class RestaurantGUI extends javax.swing.JFrame {
         getContentPane().setLayout(null);
 
         
+        //Bouton logout
+        cmdLogout.setText("Logout");
+        cmdLogout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdLogoutActionPerformed(evt);
+            }
+        });
+        cmdLogout.setBounds(40, 620, 80, 80);
+        
+        //Table 1
         cmdTable1.setText("Table 1");
-        cmdTable1.setMaximumSize(new java.awt.Dimension(100, 100));
-        cmdTable1.setMinimumSize(new java.awt.Dimension(100, 100));
-        cmdTable1.setPreferredSize(new java.awt.Dimension(100, 100));
         cmdTable1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmdTable1ActionPerformed(evt);
             }
         });
-        getContentPane().add(cmdTable1);
         cmdTable1.setBounds(430, 460, 100, 100);
         
         
+        //Table 2
         cmdTable2.setText("Table 2");
-        cmdTable2.setMaximumSize(new java.awt.Dimension(100, 100));
-        cmdTable2.setMinimumSize(new java.awt.Dimension(100, 100));
-        cmdTable2.setPreferredSize(new java.awt.Dimension(100, 100));
         cmdTable2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmdTable2ActionPerformed(evt);
             }
         });
-        getContentPane().add(cmdTable2);
         cmdTable2.setBounds(610, 460, 100, 100);
         
         
+        //Table 3
         cmdTable3.setText("Table 3");
-        cmdTable3.setMaximumSize(new java.awt.Dimension(100, 100));
-        cmdTable3.setMinimumSize(new java.awt.Dimension(100, 100));
-        cmdTable3.setPreferredSize(new java.awt.Dimension(100, 100));
         cmdTable3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmdTable3ActionPerformed(evt);
             }
         });
-        getContentPane().add(cmdTable3);
         cmdTable3.setBounds(430, 620, 100, 100);
         
 
+        //Table 4
         cmdTable4.setText("Table 4");
-        cmdTable4.setMaximumSize(new java.awt.Dimension(100, 100));
-        cmdTable4.setMinimumSize(new java.awt.Dimension(100, 100));
-        cmdTable4.setPreferredSize(new java.awt.Dimension(100, 100));
         cmdTable4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 cmdTable4ActionPerformed(evt);
             }
         });
-        getContentPane().add(cmdTable4);
         cmdTable4.setBounds(610, 620, 100, 100);
+        lblOccupe.isFocusOwner();
         
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setText("Légende");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(30, 250, 100, 40);
+        //Label légende
+        lblLegende.setFont(new java.awt.Font("Tahoma", 0, 18));
+        lblLegende.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblLegende.setText("Légende");
+        lblLegende.setBounds(30, 250, 100, 40);
 
-        jLabel3.setBackground(new java.awt.Color(255, 0, 0));
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Occupé");
-        jLabel3.setToolTipText("");
-        jLabel3.setOpaque(true);
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(40, 310, 80, 80);
+        //Label occupé
+        lblOccupe.setBackground(new java.awt.Color(255, 0, 0));
+        lblOccupe.setFont(new java.awt.Font("Tahoma", 0, 14));
+        lblOccupe.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblOccupe.setText("Occupé");
+        lblOccupe.setOpaque(true);
+        lblOccupe.setBounds(40, 310, 80, 80);
 
-        jLabel4.setBackground(new java.awt.Color(0, 255, 0));
-        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setText("Libre");
-        jLabel4.setOpaque(true);
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(40, 410, 80, 80);
+        //Label libre
+        lblLibre.setBackground(new java.awt.Color(0, 255, 0));
+        lblLibre.setFont(new java.awt.Font("Tahoma", 0, 14));
+        lblLibre.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblLibre.setText("Libre");
+        lblLibre.setOpaque(true);
+        lblLibre.setBounds(40, 410, 80, 80);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Service/Restaurant.jpg"))); // NOI18N
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 800, 790);
+        //Image background du restaurant.
+        lblImage.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Service/Restaurant.jpg")));
+        lblImage.setBounds(0, 0, 800, 790);
+        
+        //Ajoute les éléments à la fenêtre.
+        getContentPane().add(cmdLogout);
+        getContentPane().add(cmdTable1);
+        getContentPane().add(cmdTable2);
+        getContentPane().add(cmdTable3);
+        getContentPane().add(cmdTable4);
+        getContentPane().add(lblLegende);
+        getContentPane().add(lblOccupe);
+        getContentPane().add(lblLibre);
+        getContentPane().add(lblImage);
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
    
     private void setColor(){
         
@@ -134,17 +144,20 @@ public class RestaurantGUI extends javax.swing.JFrame {
         cmdTable2.setBackground(Color.RED);
         
     }
-    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {
         setColor();
-    }//GEN-LAST:event_formWindowOpened
-
+    }
     
+    private void cmdLogoutActionPerformed(java.awt.event.ActionEvent evt) {
+        System.exit(0);
+    }
+
     private void cmdTable1ActionPerformed(java.awt.event.ActionEvent evt) {
         table1.setVisible(true);
     }
 
     private void cmdTable2ActionPerformed(java.awt.event.ActionEvent evt) {
-        table2.setVisible(true);     
+        table2.setVisible(true);    
     }
 
     private void cmdTable3ActionPerformed(java.awt.event.ActionEvent evt) {
@@ -155,9 +168,7 @@ public class RestaurantGUI extends javax.swing.JFrame {
         table4.setVisible(true);
     }
     
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String args[]) {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -178,12 +189,14 @@ public class RestaurantGUI extends javax.swing.JFrame {
 
     }
     
+    //Déclaration des variables.
+    private javax.swing.JButton cmdLogout;
     private javax.swing.JButton cmdTable1;
     private javax.swing.JButton cmdTable2;
     private javax.swing.JButton cmdTable3;
     private javax.swing.JButton cmdTable4;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lblImage;
+    private javax.swing.JLabel lblLegende;
+    private javax.swing.JLabel lblOccupe;
+    private javax.swing.JLabel lblLibre;
 }
